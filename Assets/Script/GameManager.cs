3a55a15a4d935de58ee3662d8de6769eade2d1e2
Tài Lê Doãn <ledoantai2204@gmail.com>
2023-06-14 15:54:50 +0700

    + Random info map

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,12 +7,77 @@ public class GameManager : MonoBehaviour
     // Singleton instance
     private static GameManager instance;
 
+    //Map information
+    public int totalMap = 35;
+    public int numdef =2;
+    public int numSnake;
+    public int numLadder;
+    public int numNor;
+    
+    public int minQues;
+    public int maxQues;
+    
+    public int minQuesBat;
+    public int maxQuesBat;
+    
+    public int minMini;
+    public int maxMini;
 
+    public int minMiniBat;
+    public int maxMiniBat;
     // Public accessor for the singleton instance
     public static GameManager Instance
     {
         get { return instance; }
     }
+
+    public void ChangeTypeMap(int type)
+    {
+        switch (type)
+        {
+            //case 3 snake 3 ladder
+            case 0:
+                numSnake = 3;
+                numLadder = 3;
+                minQues = 4;
+                maxQues = 5;
+                minQuesBat = 2;
+                maxQuesBat = 3;
+                minMini = 4;
+                maxMini = 5;
+                minMiniBat = 2;
+                maxMiniBat = 3;
+                break;
+
+            //case 2 snake 3 ladder
+            case 1:
+                numSnake = 2;
+                numLadder = 3;
+                minQues = 4;
+                maxQues = 5;
+                minQuesBat = 3;
+                maxQuesBat = 4;
+                minMini = 4;
+                maxMini = 5;
+                minMiniBat = 3;
+                maxMiniBat = 4;
+                break;
+            //case 3 snake 2 ladder
+            case 2:
+                numSnake = 2;
+                numLadder = 3;
+                minQues = 4;
+                maxQues = 5;
+                minQuesBat = 3;
+                maxQuesBat = 4;
+                minMini = 4;
+                maxMini = 5;
+                minMiniBat = 3;
+                maxMiniBat = 4;
+                break;
+        }
+    }
+
     private void Awake()
     {
         // Check if an instance already exists
--- Assets/Script/MapController.cs
+++ Assets/Script/MapController.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class MapController : MonoBehaviour
 {
@@ -18,8 +19,23 @@ public class MapController : MonoBehaviour
                 map.transform.GetChild(i).GetComponent<Renderer>().material.color = Color.green;
             }
         }
+
+        RanMap();
     }
 
+    public void RanMap()
+    {
+        int i = 0; 
+            i = Random.Range(0, 3);
+        
+        GameManager.Instance.ChangeTypeMap(i);
+        int numQues = UnityEngine.Random.Range(GameManager.Instance.minQues, GameManager.Instance.maxQues +1);
+        int numQuesBat = UnityEngine.Random.Range(GameManager.Instance.minQuesBat, GameManager.Instance.maxQuesBat + 1);
+        int numMini = UnityEngine.Random.Range(GameManager.Instance.minMini, GameManager.Instance.maxMini + 1);
+        int numMiniBat = UnityEngine.Random.Range(GameManager.Instance.minMiniBat, GameManager.Instance.maxMiniBat + 1);
+        int numNor = GameManager.Instance.totalMap - numQues - numQuesBat - numMini - numMiniBat - GameManager.Instance.numSnake*2 - GameManager.Instance.numLadder*2- GameManager.Instance.numdef;
+
+    }
     // Update is called once per frame
     void Update()
     {
